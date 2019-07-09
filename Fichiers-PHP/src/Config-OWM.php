de051963d606dd44dd0c6be4c28573a6a0a346d6
MilesTEG1 <MilesTEG1@gmail.com>
2019-07-09 10:31:45 +0200

    Add files via upload

---
+++ Fichiers-PHP/src/Config-OWM.php
@@ -0,0 +1,13 @@
+<?php
+// Rentrer la clé d'API OpenWeaterMap ici.
+// Pour récupérer les données publique de température de la ville où vous habitez, il faut
+// avoir créé un compte en suivant la procédure décrite ici : https://openweathermap.org/appid
+// L'appel à l'API se fait avec le lien :
+//         http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+// 
+// Il faudra vérifier que ce fichier n'est pas accessible en édition, ou que le dossier src
+// ne puisse pas être listé.
+
+$omw_api_key = "";
+
+?>
---
+++ Fichiers-PHP/src/Config.php
@@ -0,0 +1,12 @@
+<?php
+// Rentrer vos données ici.
+// Il faudra récupérer le Client_ID et le Client-Secret ici : https://dev.netatmo.com/myaccount/
+// Il faut créer une application dans l'interface web, et les deux infos seront créées.
+// Il faudra vérifier que ce fichier n'est pas accessible en édition, ou que le dossier src
+// ne puisse pas être listé.
+
+$client_id = "";
+$client_secret = "";
+$test_username = "";
+$test_password = "";
+?>
